1c29bd9f3c1b38d6000b8e81621ddc1017ec85af
ziankork <mynameiszian@gmail.com>
2024-09-16 14:40:27 +0800

    fix: fix remappings

--- test/Helper.sol
+++ test/Helper.sol
@@ -5,10 +5,13 @@ import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
-import Helpers
+import "forge-std/mocks/MockERC20.sol";
+import "Depeg-swap/test/forge/Helper.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 
-contract HookHelpers is Deployers {
+contract HookHelpers is Deployers, Helper {
     DepegHook internal hook;
+    using CurrencyLibrary for Currency;
 
     uint160 internal constant HOOK_ADDRESS = uint160(
         Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
@@ -19,6 +22,10 @@ contract HookHelpers is Deployers {
     Currency internal token1;
 
     function setupWithMockPair() internal {
+        address mock1 = address(deployMockERC20("MOCK1", "MCK", 18));
+        address mock2 = address(deployMockERC20("MOCK2", "MCK", 18));
+
+        token0 = mock0 < mock1 ? Currency.wrap(mock0) : Currency.wrap(mock1);
         
     }
 }
